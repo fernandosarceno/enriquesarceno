2784d9880eca4c9b5b73311c14e925abc751ce83
Fernando Enriquez Sarceño Zuñiga (24145) <fernando.sarceno@est.zamorano.edu>
2022-09-11 20:25:35 -0600

    08.09.22

---
+++ labs/lab02/pagoporhora.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+
+horastrabajadas = input ("Ingrese las horas trabajadas: ")
+valorHora = input("Ingrese el valor por hora: ")
+pagoHora = int(horastrabajadas) * float(valorHora)
+print(pagoHora)
